91f2e769fd771fd4aacf99a42539345e599a0fae
danny yan <danny.yan@gmail.com>
2019-06-17 22:19:35 -0700

    Edit and Delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
 import AddUserForm from './forms/AddUserForm'
+import EditUserForm from './forms/EditUserForm'
 const App = () => {
   const usersData = [
     { id: 1, name: 'Danny', username: 'RoA'}, 
@@ -22,6 +23,7 @@ const App = () => {
   }
   
   const deleteUser = id => {
+    setEditing(false);
     setUsers(users.filter(user => user.id !== id));
   }
 
@@ -31,15 +33,33 @@ const App = () => {
     setCurrentUser({id: user.id, name: user.name, username: user.username});
   }
 
-  const updateUser 
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+
+    setUsers(users.map(user => (user.id === id ? updatedUser: user)))
+  }
 
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
       <div className="flex-row">
         <div className="flex-large">
-          <h2>Add user</h2>
-          <AddUserForm addUser={addUser}/>
+          {editing ? (
+            <div>
+              <h2>Edit user</h2>
+              <EditUserForm
+                editing={editing}
+                setEditing={setEditing}
+                currentUser={currentUser}
+                updateUser={updateUser}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add user</h2>
+              <AddUserForm addUser={addUser}/>
+            </div>
+        )}
         </div>
         <div className="flex-large">
           <h2>View users</h2>
